f7481aaaeaffc0a3d556b2ae0e7819fba55ec68e
Christopher <christopher.petronella@gmail.com>
2020-11-20 13:15:49 -0500

    commit tree 76d155a10dfce80e86201f2dcf0fbb48784dc8f6

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+def my_collect(languages)
+    collection = []
 
-
+    i = 0
+    while i < languages.length
+        collection << yield(languages[i]) 
+        i += 1
+    end
+    collection
+end
